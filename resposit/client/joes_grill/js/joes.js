8bb5515758d577c0404d0ae6248781df47977887
grilldemo <ryanmcnicholas7@gmail.com>
2025-09-23 20:15:02 -0400

    made fixes

---
+++ resposit/client/joes_grill/js/joes.js
@@ -0,0 +1,5 @@
+var item_name;
+var item_price;
+var item_calorie;
+var item_rating;
+var item_purchesed;
--- resposit/joes_grill/js/joes.js
+++
@@ -1,6 +0,0 @@
-var order_id;
-var order_item_id;
-var item_id;
-var item_quantity;
-var card_num;
-var address;
