c5a0180f1354758a7e11fb4e1d8555d181d036fc
grilldemo <ryanmcnicholas7@gmail.com>
2025-10-29 20:45:34 -0400

    big change

---
+++ resposit/server/app.js
@@ -0,0 +1,29 @@
+const express=require('express');
+const cors=require('cors');
+const path=require('path');
+const bodyParser=require('body-parser');
+
+const app=express();
+
+app.use(cors());
+
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin','*');
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use("/client",express.static(path.resolve(__dirname +"/../client/")));
+
+const port=5000;
+//page listeners(our router)
+
+//service listeners(our data procceses)
+
+//listen
+var server=app.listen(port,function(err){
+    if(err) throw err;
+
+    console.log("listening on port "+port);
+})
